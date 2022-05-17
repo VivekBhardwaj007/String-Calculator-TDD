4f480f52e653515841575b380e980be8a7862871
Vivek <vivekbhardwaj425@gmail.com>
2022-05-17 17:56:00 +0530

    Updating on Test cases and Code refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,59 +6,124 @@ describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
+  // START : configuration of test scripts
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
+  // END : configuration of test scripts
 
+  // START : running before every test script
   beforeEach(() => {
+    // TestBed is main utility which will use for configuration
     fixture = TestBed.createComponent(AppComponent);
+
+    // Getting instance of component
     app = fixture.componentInstance;
+
+    // Detecting changes
     fixture.detectChanges();
   });
+  // END : running before every test script
 
+  // START : chekcing Component Initiated or not
   it('Component Initiated', () => {
     expect(app).toBeTruthy();
   });
+  // END : chekcing Component Initiated or not
 
-  it("should return zero when '' is passed", () => {
-    expect(app.add('')).toEqual(0);
+  // START : New group of test scripts
+  // Basic functionalities here
+  describe('Basic Functionality', () => {
+    // START : passing blank string
+    it("should return zero when '' is passed", () => {
+      expect(app.add('')).toEqual(0);
+    });
+    // END : passing blank string
+
+    // START : Passing only single number string
+    it('should return the number itself when a single number is passed', function () {
+      expect(app.add('1')).toEqual(1);
+    });
+    // START : Passing only single string
+
+    // START : Passing 2 number string
+    it('should return the sum of the numbers if two are given', function () {
+      expect(app.add('1,2')).toEqual(3);
+    });
+    // END : Passing 2 number string
+
+    // START : Paasing more than 2 number string
+    it('should return the sum of the numbers if more than two are given', function () {
+      expect(app.add('1,2,3')).toEqual(6);
+    });
+    // END : Paasing more than 2 number string
   });
+  // END : New group of test scripts
 
-  it('should return the number itself when a single number is passed', function () {
-    expect(app.add('1')).toEqual(1);
+  // START : New group of test scripts
+  // Unknow AMount of Number - dynamic
+  describe('Unknow Amount OF Number', () => {
+    it('should return the sum of an unknown amount of numbers', function () {
+      // Function declaration
+      // Accpt 2 params 1. length, 2. maximum number
+      let array = (len: number, maximum: number) =>
+        [...new Array(len)].map(() => Math.round(Math.random() * maximum));
+
+      // Generating radom number and passing into arrray function
+      let result = array(
+        Math.floor(Math.random() * 100 + 1),
+        Math.floor(Math.random() * 100 + 1)
+      );
+
+      // Getting sum of every number
+      let sum = result.reduce((a, b) => a + b, 0);
+
+      // Converting array into string
+      let argument = result.join();
+
+      // Checking Values
+      // If expectation matched with result then passed.
+      expect(app.add(argument)).toEqual(sum);
+    });
   });
+  // END : New group of test scripts
 
-  it('should return the sum of the numbers if two are given', function () {
-    expect(app.add('1,2')).toEqual(3);
+  // START : New group of test scripts
+  // Complex seperator added in string
+  describe('Complex Delimiters', () => {
+    // START : Passing value with 'enter' instead of comma
+    it('should allow \\n in between the input number string', function () {
+      expect(app.add('1\n2,3')).toEqual(6);
+    });
+    // END : Passing value with 'enter' instead of comma
+
+    // START : Checking string with sperator
+    it('should allow //;\n in between the input string', function () {
+      expect(app.add('//;\n1;2')).toEqual(3);
+    });
+    // END : Checking string with sperator
+
+    // START : Checking string with more complex sperator
+    it('#add should allow multiple, arbitrary length user-defined separators', function () {
+      expect(app.add('//[*][%]\n1*2%3')).toBe(6);
+    });
+    // END : Checking string with more complex sperator
   });
+  // END : New group of test scripts
 
-  it('should return the sum of the numbers if more than two are given', function () {
-    expect(app.add('1,2,3')).toEqual(6);
+  // START : New group of test scripts
+  // Handle Invalid Inputs
+  describe('Handle Invalid Inputs', () => {
+    // START : Passing negative numbers in string
+    it('should not allow negative numbers', function () {
+      expect(() => {
+        app.add('-1,2.3');
+      }).toThrow(new Error('Negatives not allowed!'));
+    });
+    // END : Passing negative numbers in string
   });
-
-  it('should return the sum of an unknown amount of numbers', function () {
-    let array = (len: number, maximum: number) =>
-      [...new Array(len)].map(() => Math.round(Math.random() * maximum));
-
-    let random_number = Math.floor(Math.random() * 100 + 1);
-    let result = array(random_number, random_number);
-
-    let sum = result.reduce((a, b) => a + b, 0);
-
-    let argument = result.join();
-    expect(app.add(argument)).toEqual(sum);
-  });
-
-  it('should allow \\n in between the input number string', function () {
-    expect(app.add('1\n2,3')).toEqual(6);
-  });
-
-  it('should not allow negative numbers', function () {
-    expect(() => {
-      app.add('-1,2.3');
-    }).toThrow(new Error('Negatives not allowed!'));
-  });
+  // END : New group of test scripts
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test';
+  // START : Regular Expression for replacing seperators
+  regularExpression(regExpString: any) {
+    regExpString = regExpString.replace(
+      /[\-\/\{\}\(\)\*\+\?\.\\\^\$\|]/g,
+      '\\$&'
+    );
 
+    // Replacing
+    // '][' -> |
+    // | -> ''
+    return regExpString.replace('][', '|').replace(/\[|\]/g, '');
+  }
+  // END : Regular Expression for replacing seperators
+
+  // START : Extrating integer values
+  extractIntegerValues(input: any) {
+    // initialize variable
+    let numbers = input;
+
+    // Defining delimiter
+    let delimiter = /[,\n]/;
+
+    // checking input start with '//'
+    if (input.startsWith('//')) {
+      // Updating delimiter with new regular expression
+      delimiter = new RegExp(
+        this.regularExpression(input.split('\n').shift().substr(2))
+      );
+      numbers = input.replace(/^\/\/.+\n/, '');
+    }
+
+    // START : converting value in to numbers
+    // spliting values using delimiter
+    // using map function converting values in numbers
+    let integerValues = numbers.split(delimiter).map(function (value: any) {
+      // value conversion
+      let integerValue = parseInt(value);
+
+      return integerValue;
+    });
+
+    // returning final all values
+    return integerValues;
+  }
+  // END : Extrating integer values
+
+  // START : add function for addition which will accept -
+  // 1. String Numbers
+  // 2. No negative number
   add(numbers: string) {
-    let result: number;
-    let addition_array = numbers.replace(/\r?\n/g, ',').split(',').map(Number);
+    // String conversion
+    // 1. Removing enter with comma ',', '*' etc etc
+    // 2. Spliting all numbers with comma ','
+    // 3. Converting all string numbers into 'Numnber'
+    // 4. Assignning data into a variable
+    let addition_array = this.extractIntegerValues(numbers);
 
-    if (addition_array.some((value) => value < 0)) {
+    // Checking array using 'some' method where it will return true or false.
+    if (addition_array.some((value: number) => value < 0)) {
+      // Throwing a error for Negative number.
       throw new Error('Negatives not allowed!');
     }
 
-    if (numbers === '') {
+    // checking if there any blank string
+    // It should return 0
+    if (!numbers || numbers === '') {
       return 0;
-    } else if (numbers.length === 1) {
-      result = parseInt(numbers);
     } else {
-      result = addition_array.reduce((a, b) => a + b);
+      // Returning addition of every value
+      return addition_array.reduce((a: any, b: any) => a + b);
     }
-
-    return result;
   }
+  // END : add function for addition
 }
